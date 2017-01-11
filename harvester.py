0c7b5bc0e7fdaabc35b753f0c05cf8f31369a215
aesthese <lostintherush@gmail.com>
2017-01-11 04:12:54 +0100

    Update harvester.py

--- harvester.py
+++ harvester.py
@@ -4,7 +4,7 @@ Harvests all comments from all posts on a Facebook page and writes
 them to a file. No metadata, just raw comment text separated by \n.
 I don't know why the sum of all "total_count" fields returned from
 the FB API, are greater than the number of comments we are able to
-get. It confuses me and now this project bores me. Fork away.
+download. It confuses me and now this project bores me. Fork away.
 
 """
 
